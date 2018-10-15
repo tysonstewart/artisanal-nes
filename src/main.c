52bb09f79069fe5f3f7968fb3c998ec2a5dbea42
Tyson Stewart <tjstewart.npm@gmail.com>
2018-10-15 16:41:15 -0500

    Add debug function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include "neslib.h"
 #include "render.c"
+#include "debug.c"
 
 void main(void)
 {
 	draw_title_screen();
+	write_debug("IT'S SOMETHING!");
 	while(1)//infinite loop, title-gameplay
 	{
 	}
